d1b598f79aa0fe2c38e15e9fa994c619c844e296
papesocendiaye <pape.soce.ndiaye@outlook.com>
2024-03-27 21:25:25 +0000

    maj de model/employe et de store/EmployeStore

--- esp/dstib/drugmanagement/model/Employe.java
+++ esp/dstib/drugmanagement/model/Employe.java
@@ -1,4 +1,39 @@
 package esp.dstib.drugmanagement.model;
 
 public class Employe  {
+    private int id;
+    private String prenom;
+    private String nom;
+
+    // Constructeur
+    public Employe(int id, String prenom, String nom, ) {
+        this.id = id;
+        this.prenom = prenom;
+        this.nom = nom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
 }
+
--- esp/dstib/drugmanagement/store/EmployeStore.java
+++ esp/dstib/drugmanagement/store/EmployeStore.java
@@ -5,22 +5,140 @@ import java.util.List;
 
 public class EmployeStore {
 
-    public List<Employe> getAll () {
-        return null;
+    public List<Employe> getAll () throws RuntimeExeption {
+        Connection connection = ConnectionDB.getConnection();
+        if (connection == null) { //gerer ca avec exeptions
+            System.err.println("La connexion n'est pas initialisée.");
+            return 0;
+        }
+        String sql="SELECT id, prenom, nom FROM employes";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultatSet.next()) {
+                int id = resultatSet.getInt("id");
+                String prenom = resultatSet.getString("prenom");
+                String nom = resultatSet.getString("nom");
+                System.out.println("ID: " + id + ", Prénom: " + prenom + ", Nom: " + nom);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return 0;
     }
 
     public Employe get (int id) {
-        return null;
+
+        Connection connection = ConnectionDB.getConnection();
+        if (connection == null) { //gerer ca avec exeptions
+            System.err.println("La connexion n'est pas initialisée.");
+            return 0;
+        }
+        String sql = "SELECT * FROM employes WHERE id = ?";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultatSet.next()) {
+                int id = resultatSet.getInt(1);
+                String prenom = resultatSet.getString(2);
+                String nom = resultatSet.getString(3);
+                System.out.println("ID: " + id + ", Prénom: " + prenom + ", Nom: " + nom);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return 0;
     }
+
 
     public void delete (int id) {
+        Connection connection = ConnectionDB.getConnection();
+        if (connection == null) { //gerer ca avec exeptions
+            System.err.println("La connexion n'est pas initialisée.");
+            return 0;
+        }
+        String sql = "DELETE FROM employes WHERE id = ?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql)
+            statement.setInt(1, id);
+            int suppr = statement.executeUpdate();
+            if (suppr > 0) {
+                System.out.println("Employé avec l'ID " + id + " supprimé avec succès !");
+                return true;
+            } else {
+                System.out.println("Aucun employé trouvé avec l'ID " + id + ". Aucune suppression n'a été effectuée.");
+                return false;
+            }
+        }catch (Exception e){
+            throw new RuntimeException();
+        }
     }
 
-    public void add () {
+    public void add () throws RuntimeException {
+        Connection connection = ConnectionDB.getConnection();
+        // Vérifier si la connexion est null
+        if (connection == null) { //gerer ca avec exeptions
+            System.err.println("La connexion n'est pas initialisée.");
+            return 0;
+        }
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Veuillez saisir le nom de l'employé : ");
+            String nom = scanner.nextLine();
+            System.out.println("Veuillez saisir le prenom de l'employé : ");
+            String prenom = scanner.nextLine();
+            scanner.close();
+
+        }catch(Exception e) { //thow l'exception correspondante
+            e.printStackTrace();
+        }
+        // insertion
+        String sql = "INSERT INTO employes (nom, prenom) VALUES (?, ?)";
+        try {
+            PreparedStatement statement = connection.preparedStatement(sql);
+            pstmt.setString(2, nom);
+            pstmt.setString(3, prenom);
+            pstmt.executeUpdate();
+            System.out.println("Employé ajouté avec succès !");
+            return 1;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return 0;
     }
 
     public Employe modify (int id, Employe employe) {
-        return null;
-    }
+
+        Connection connection = ConnectionDB.getConnection();
+        // Vérifier si la connexion est null
+        if (connection == null) { //gerer ca avec exeptions
+            System.err.println("La connexion n'est pas initialisée.");
+            return 0;
+        }
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Veuillez saisir le nom de l'employé : ");
+
+            System.out.println("Veuillez saisir le prenom de l'employé : ");
+            employe.setPrenom(scanner.nextLine());
+            scanner.close();
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        // insertion
+        String sql = "UPDATE employes SET nom = ?, prenom = ? WHERE id = ?";
+        try {
+            PreparedStatement statement = connection.preparedStatement(sql);
+            pstmt.setString(2, employe.getNom());
+            pstmt.setString(3, employe.getPrenom());
+            pstmt.executeUpdate();
+            System.out.println("Employé ajouté avec succès !");
+            return 1;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return 0;
 
 }

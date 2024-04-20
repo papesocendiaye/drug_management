1d81da0c00989945e22d23a56ef63405312ed173
soxnarama <ramatoulayef56@gmail.com>
2024-04-20 00:00:13 +0000

    managercontroller

--- esp/dstib/drugmanagement/controllers/ManagerController.java
+++ esp/dstib/drugmanagement/controllers/ManagerController.java
@@ -1,8 +1,10 @@
 package esp.dstib.drugmanagement.controllers;
 import esp.dstib.drugmanagement.core.CredentialManagement;
 import esp.dstib.drugmanagement.core.EmployeManagement;
+import esp.dstib.drugmanagement.core.Tools;
 import esp.dstib.drugmanagement.menus.MenuManager;
 import esp.dstib.drugmanagement.model.Credencial;
+import esp.dstib.drugmanagement.model.Drug;
 import esp.dstib.drugmanagement.model.Employe;
 
 public class ManagerController {
@@ -10,12 +12,33 @@ public class ManagerController {
 
     private final static EmployeManagement employeManagement = new EmployeManagement();
     private final static CredentialManagement credencialMangement = new CredentialManagement();
+    private final static MenuManager menuManager = new MenuManager();
 
+    public static void menu (Employe employe, Drug drug) {
+        boolean isLoged = true;
+        while (isLoged) {
+            int choice= Tools.inputId("appuyer 1 pour la gestion d'employé, ou 2 pour la gestion de medicament");
+            
+            switch (choice) {
+                case 1:
+                    MenuManager.crudEmploye(employe);
+
+                    break;
+                case 2:
+                    menuManager.crudMed(drug);
+                    break;  
+                case 3:
+                    isLoged = false;
+                    break;
+            }
+        }
+    } // le reste cest pas bon  le programme  doit verifier quel choix il a fait puis lui afficher le menu la
+    //refaire la class app aussi
     public static void manage (Employe employe) {
         boolean isLoged = true;
         while (isLoged) {
             
-            Integer choice = MenuManager.showMainMenu(employe);
+            Integer choice = MenuManager.crudEmploye(employe);
             switch (choice) {
                 case 1:
                     Credencial credencial = credencialMangement.createCredencial();
--- esp/dstib/drugmanagement/menus/MenuManager.java
+++ esp/dstib/drugmanagement/menus/MenuManager.java
@@ -1,16 +1,24 @@
 package esp.dstib.drugmanagement.menus;
 
 import esp.dstib.drugmanagement.core.Tools;
+import esp.dstib.drugmanagement.model.Drug;
 import esp.dstib.drugmanagement.model.Employe;
 
 public class MenuManager {
 
-    public static Integer showMainMenu (Employe employe) {
+    public static Integer crudEmploye (Employe employe) {
         Tools.print("Bienvenue "+employe.getPrenom()+" "+employe.getNom());
         Tools.print("MENU PRINCIPAL");
         Tools.print("1 - Ajouter un employé");
         Tools.print("2 - Modifier un employé");
         Tools.print("3 - Supprimer un employé");
+        Tools.print("4 - quitter");
+        return Integer.parseInt( Tools.input(">") ) ;
+    }
+    public Integer crudMed (Drug drug) {
+        Tools.print("MENU PRINCIPAL");
+        Tools.print("1 - Ajouter un medicament");
+        Tools.print("3 - Supprimer un medicament");
         Tools.print("4 - quitter");
         return Integer.parseInt( Tools.input(">") ) ;
     }
